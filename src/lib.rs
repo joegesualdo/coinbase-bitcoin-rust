b9ea73eba9ecbbea74f58f5e433b1ed86f490592
Joe Gesualdo <joegesualdo@gmail.com>
2022-12-09 15:10:21 -0500

    Formatting change

--- src/lib.rs
+++ src/lib.rs
@@ -20,16 +20,19 @@ pub fn get_price_data() -> Result<PriceData> {
     let price_data = PriceData { spot, buy, sell };
     Ok(price_data)
 }
+
 pub fn get_spot_price() -> Result<Price> {
     let response: api::v2::SpotPriceResponse = api::v2::request_spot_price()?;
     let price: f32 = response.data.amount.parse()?;
     return Ok(price);
 }
+
 pub fn get_buy_price() -> Result<Price> {
     let response: api::v2::BuyPriceResponse = api::v2::request_buy_price()?;
     let price: f32 = response.data.amount.parse()?;
     return Ok(price);
 }
+
 pub fn get_sell_price() -> Result<Price> {
     let response: api::v2::SellPriceResponse = api::v2::request_sell_price()?;
     let price: f32 = response.data.amount.parse()?;
